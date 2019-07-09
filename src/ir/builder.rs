edbd660e54674469cd961cf76a86396aac6df5d9
maekawatoshiki <konndennsa@gmail.com>
2019-07-09 22:39:57 +0900

    Refactor

--- src/ir/builder.rs
+++ src/ir/builder.rs
@@ -38,182 +38,79 @@ impl<'a> Builder<'a> {
 
     pub fn build_alloca(&mut self, ty: Type) -> Value {
         let ptr_ty = ty.get_pointer_ty();
-        let instr = Instruction::new(
-            Opcode::Alloca(ty),
-            ptr_ty,
-            self.function_ref_mut().next_vreg(),
-        );
-        let instr_id = self.function_ref_mut().instr_id(instr);
-        let val = Value::Instruction(InstructionValue {
-            func_id: self.func_id,
-            id: instr_id,
-        });
-        let bb = self.cur_bb.unwrap();
-        self.function_ref_mut()
-            .basic_block_ref_mut(bb)
-            .iseq
-            .push_back(val);
-        val
+        let instr = self.create_instr_value(Opcode::Alloca(ty), ptr_ty);
+        self.append_instr_to_cur_bb(instr);
+        instr
     }
 
     pub fn build_load(&mut self, v: Value) -> Value {
-        let ty = v.get_type(self.module).get_element_ty().unwrap().clone();
-        let instr = Instruction::new(Opcode::Load(v), ty, self.function_ref_mut().next_vreg());
-        let instr_id = self.function_ref_mut().instr_id(instr);
-        let val = Value::Instruction(InstructionValue {
-            func_id: self.func_id,
-            id: instr_id,
-        });
-        let bb = self.cur_bb.unwrap();
-        self.function_ref_mut()
-            .basic_block_ref_mut(bb)
-            .iseq
-            .push_back(val);
-        val
+        let instr = self.create_instr_value(
+            Opcode::Load(v),
+            v.get_type(self.module).get_element_ty().unwrap().clone(),
+        );
+        self.append_instr_to_cur_bb(instr);
+        instr
     }
 
     pub fn build_store(&mut self, src: Value, dst: Value) -> Value {
-        let instr = Instruction::new(
-            Opcode::Store(src, dst),
-            Type::Void,
-            self.function_ref_mut().next_vreg(),
-        );
-        let instr_id = self.function_ref_mut().instr_id(instr);
-        let val = Value::Instruction(InstructionValue {
-            func_id: self.func_id,
-            id: instr_id,
-        });
-        let bb = self.cur_bb.unwrap();
-        self.function_ref_mut()
-            .basic_block_ref_mut(bb)
-            .iseq
-            .push_back(val);
-        Value::None
+        let instr = self.create_instr_value(Opcode::Store(src, dst), Type::Void);
+        self.append_instr_to_cur_bb(instr);
+        instr
     }
 
     pub fn build_add(&mut self, v1: Value, v2: Value) -> Value {
-        let ty = v1.get_type(self.module).clone();
-        let instr = Instruction::new(Opcode::Add(v1, v2), ty, self.function_ref_mut().next_vreg());
-        let instr_id = self.function_ref_mut().instr_id(instr);
-        let val = Value::Instruction(InstructionValue {
-            func_id: self.func_id,
-            id: instr_id,
-        });
-        let bb = self.cur_bb.unwrap();
-        self.function_ref_mut()
-            .basic_block_ref_mut(bb)
-            .iseq
-            .push_back(val);
-        val
+        let instr = self.create_instr_value(Opcode::Add(v1, v2), v1.get_type(self.module).clone());
+        self.append_instr_to_cur_bb(instr);
+        instr
     }
 
     pub fn build_sub(&mut self, v1: Value, v2: Value) -> Value {
-        let ty = v1.get_type(self.module).clone();
-        let instr = Instruction::new(Opcode::Sub(v1, v2), ty, self.function_ref_mut().next_vreg());
-        let instr_id = self.function_ref_mut().instr_id(instr);
-        let val = Value::Instruction(InstructionValue {
-            func_id: self.func_id,
-            id: instr_id,
-        });
-        let bb = self.cur_bb.unwrap();
-        self.function_ref_mut()
-            .basic_block_ref_mut(bb)
-            .iseq
-            .push_back(val);
-        val
+        let instr = self.create_instr_value(Opcode::Sub(v1, v2), v1.get_type(self.module).clone());
+        self.append_instr_to_cur_bb(instr);
+        instr
     }
 
     pub fn build_mul(&mut self, v1: Value, v2: Value) -> Value {
-        let ty = v1.get_type(self.module).clone();
-        let instr = Instruction::new(Opcode::Mul(v1, v2), ty, self.function_ref_mut().next_vreg());
-        let instr_id = self.function_ref_mut().instr_id(instr);
-        let val = Value::Instruction(InstructionValue {
-            func_id: self.func_id,
-            id: instr_id,
-        });
-        let bb = self.cur_bb.unwrap();
-        self.function_ref_mut()
-            .basic_block_ref_mut(bb)
-            .iseq
-            .push_back(val);
-        val
+        let instr = self.create_instr_value(Opcode::Mul(v1, v2), v1.get_type(self.module).clone());
+        self.append_instr_to_cur_bb(instr);
+        instr
     }
 
     pub fn build_rem(&mut self, v1: Value, v2: Value) -> Value {
-        let ty = v1.get_type(self.module).clone();
-        let instr = Instruction::new(Opcode::Rem(v1, v2), ty, self.function_ref_mut().next_vreg());
-        let instr_id = self.function_ref_mut().instr_id(instr);
-        let val = Value::Instruction(InstructionValue {
-            func_id: self.func_id,
-            id: instr_id,
-        });
-        let bb = self.cur_bb.unwrap();
-        self.function_ref_mut()
-            .basic_block_ref_mut(bb)
-            .iseq
-            .push_back(val);
-        val
+        let instr = self.create_instr_value(Opcode::Rem(v1, v2), v1.get_type(self.module).clone());
+        self.append_instr_to_cur_bb(instr);
+        instr
     }
 
     pub fn build_icmp(&mut self, kind: ICmpKind, v1: Value, v2: Value) -> Value {
-        let instr = Instruction::new(
-            Opcode::ICmp(kind, v1, v2),
-            Type::Int1,
-            self.function_ref_mut().next_vreg(),
-        );
-        let instr_id = self.function_ref_mut().instr_id(instr);
-        let val = Value::Instruction(InstructionValue {
-            func_id: self.func_id,
-            id: instr_id,
-        });
-        let bb = self.cur_bb.unwrap();
-        self.function_ref_mut()
-            .basic_block_ref_mut(bb)
-            .iseq
-            .push_back(val);
-        val
+        let instr = self.create_instr_value(Opcode::ICmp(kind, v1, v2), Type::Int1);
+        self.append_instr_to_cur_bb(instr);
+        instr
     }
 
     pub fn build_br(&mut self, dst_id: BasicBlockId) -> Value {
-        let instr = Instruction::new(
-            Opcode::Br(dst_id),
-            Type::Void,
-            self.function_ref_mut().next_vreg(),
-        );
-        let instr_id = self.function_ref_mut().instr_id(instr);
-        let val = Value::Instruction(InstructionValue {
-            func_id: self.func_id,
-            id: instr_id,
-        });
+        let instr = self.create_instr_value(Opcode::Br(dst_id), Type::Void);
+        self.append_instr_to_cur_bb(instr);
+
         let cur_bb_id = self.cur_bb.unwrap();
-
-        let cur_bb = self.function_ref_mut().basic_block_ref_mut(cur_bb_id);
-        cur_bb.iseq.push_back(val);
-        cur_bb.succ.push(dst_id);
-
+        self.function_ref_mut()
+            .basic_block_ref_mut(cur_bb_id)
+            .succ
+            .push(dst_id);
         self.function_ref_mut()
             .basic_block_ref_mut(dst_id)
             .pred
             .push(cur_bb_id);
 
-        Value::None
+        instr
     }
 
     pub fn build_cond_br(&mut self, cond: Value, bb1: BasicBlockId, bb2: BasicBlockId) -> Value {
-        let instr = Instruction::new(
-            Opcode::CondBr(cond, bb1, bb2),
-            Type::Void,
-            self.function_ref_mut().next_vreg(),
-        );
-        let instr_id = self.function_ref_mut().instr_id(instr);
-        let val = Value::Instruction(InstructionValue {
-            func_id: self.func_id,
-            id: instr_id,
-        });
-        let cur_bb_id = self.cur_bb.unwrap();
+        let instr = self.create_instr_value(Opcode::CondBr(cond, bb1, bb2), Type::Void);
+        self.append_instr_to_cur_bb(instr);
 
+        let cur_bb_id = self.cur_bb.unwrap();
         let cur_bb = self.function_ref_mut().basic_block_ref_mut(cur_bb_id);
-        cur_bb.iseq.push_back(val);
         cur_bb.succ.push(bb1);
         cur_bb.succ.push(bb2);
 
@@ -226,66 +123,51 @@ impl<'a> Builder<'a> {
             .pred
             .push(cur_bb_id);
 
-        Value::None
+        instr
     }
 
     pub fn build_phi(&mut self, pairs: Vec<(Value, BasicBlockId)>) -> Value {
         let ty = pairs.get(0).unwrap().0.get_type(self.module).clone();
-        let instr = Instruction::new(Opcode::Phi(pairs), ty, self.function_ref_mut().next_vreg());
-        let instr_id = self.function_ref_mut().instr_id(instr);
-        let val = Value::Instruction(InstructionValue {
-            func_id: self.func_id,
-            id: instr_id,
-        });
-        let bb = self.cur_bb.unwrap();
-        self.function_ref_mut()
-            .basic_block_ref_mut(bb)
-            .iseq
-            .push_back(val);
-        val
+        let instr = self.create_instr_value(Opcode::Phi(pairs), ty);
+        self.append_instr_to_cur_bb(instr);
+        instr
     }
 
     pub fn build_call(&mut self, f: Value, args: Vec<Value>) -> Value {
-        let ty = f
-            .get_type(&self.module)
-            .get_function_ty()
-            .unwrap()
-            .ret_ty
-            .clone();
-        let instr = Instruction::new(
+        let instr = self.create_instr_value(
             Opcode::Call(f, args),
-            ty,
-            self.function_ref_mut().next_vreg(),
+            f.get_type(&self.module)
+                .get_function_ty()
+                .unwrap()
+                .ret_ty
+                .clone(),
         );
-        let instr_id = self.function_ref_mut().instr_id(instr);
-        let val = Value::Instruction(InstructionValue {
-            func_id: self.func_id,
-            id: instr_id,
-        });
-        let bb = self.cur_bb.unwrap();
-        self.function_ref_mut()
-            .basic_block_ref_mut(bb)
-            .iseq
-            .push_back(val);
-        val
+        self.append_instr_to_cur_bb(instr);
+        instr
     }
 
     pub fn build_ret(&mut self, v: Value) -> Value {
-        let instr = Instruction::new(
-            Opcode::Ret(v),
-            Type::Void,
-            self.function_ref_mut().next_vreg(),
-        );
+        let instr = self.create_instr_value(Opcode::Ret(v), Type::Void);
+        self.append_instr_to_cur_bb(instr);
+        instr
+    }
+
+    // Utils
+
+    fn create_instr_value(&mut self, opcode: Opcode, ret_ty: Type) -> Value {
+        let instr = Instruction::new(opcode, ret_ty, self.function_ref_mut().next_vreg());
         let instr_id = self.function_ref_mut().instr_id(instr);
-        let val = Value::Instruction(InstructionValue {
+        Value::Instruction(InstructionValue {
             func_id: self.func_id,
             id: instr_id,
-        });
+        })
+    }
+
+    fn append_instr_to_cur_bb(&mut self, instr: Value) {
         let bb = self.cur_bb.unwrap();
         self.function_ref_mut()
             .basic_block_ref_mut(bb)
             .iseq
-            .push_back(val);
-        Value::None
+            .push_back(instr);
     }
 }
