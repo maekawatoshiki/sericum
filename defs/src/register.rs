95bf1516e8a7241bd9ddfa9dd3cca428ac7a69a3
maekawatoshiki <konndennsa@gmail.com>
2020-06-23 19:41:09 +0900

    defs: Prepare to implement register definition macro

--- defs/src/register.rs
+++ defs/src/register.rs
@@ -1,10 +1,46 @@
 use proc_macro::TokenStream;
+use proc_macro_error::{
+    proc_macro2::{Group, Ident, Span},
+    *,
+};
 use proc_quote::quote;
+use syn::parse::{Parse, ParseStream};
+use syn::{parse_macro_input, token, Error, LitInt, Token};
+
+type TS = proc_macro2::TokenStream;
+
+// class GR32 (64, Int64) : GR64 {
+//      EAX,
+//      ECX,
+//      ...,
+//      R15D,
+// }
+
+struct RegisterClass {
+    name: String,
+    bit: i32,
+    ty: TS,
+    super_rc: String,
+    body: RegisterList,
+}
+
+struct RegisterList(pub Vec<Ident>);
+
+// gp_order GR32 { EAX, ... }
+// arg_order GR32 { EDI, ... }
+
+struct RegisterOrder {
+    kind: RegisterOrderKind,
+}
+
+enum RegisterOrderKind {
+    GeneralPurpose,
+    Argument,
+}
 
 // RegisterClass GR32 (i32) {
 //    EAX, EBX, .... }
 pub fn run(_item: TokenStream) -> TokenStream {
-
     let expanded = quote! {
 
     #[derive(Debug, Clone, Copy, Hash, PartialEq)]
