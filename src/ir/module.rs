902af2597b9eba9916a1098659581741495cf08d
Drumato <drumatech109@gmail.com>
2020-07-16 18:57:37 +0900

    add some doc-comments in module.rs

--- src/ir/module.rs
+++ src/ir/module.rs
@@ -2,6 +2,7 @@ use super::{function::*, types::*, DumpToString};
 use id_arena::*;
 use std::fmt;
 
+/// A representation of Module, that has name, some functions and type definisions.
 #[derive(Clone)]
 pub struct Module {
     pub name: String,
@@ -10,6 +11,22 @@ pub struct Module {
 }
 
 impl Module {
+    /// Creates an empty `Module`.
+    ///
+    /// # Arguments
+    ///
+    /// * `name` ... `Module::new()` names the created module it.
+    ///
+    /// Initialized module is really "empty", so it has no functions yet.
+    /// See [create_function()](method.create_function.html) when you want to create some functions in module.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use cilk::module::Module;
+    /// let m = Module::new("sample");
+    /// assert_eq!(m.functions.len(), 0);
+    /// ```
     pub fn new(name: &str) -> Self {
         Self {
             name: name.to_string(),
@@ -18,6 +35,25 @@ impl Module {
         }
     }
 
+    /// allocate a [Function](../function/struct.Function.html) and insert into the method's receiver(Module).
+    /// The owner of the created functions is also receiver,
+    /// so the function will be freed when the owner drops.
+    ///
+    /// # Arguments
+    ///
+    /// * `name` ... function's name
+    /// * `ret_ty` ... a [Type](../types/struct.Type.html) that the function returns.
+    /// * `params_ty` ... a sequence of [Type](../types/struct.Type.html) that are passed to function as parameters.
+    ///
+    /// # Examples
+    /// ```
+    /// use cilk::{module::Module, types::Type};
+    ///
+    /// let mut m1 = Module::new("owner");
+    /// let m2 = Module::new("other");
+    ///
+    /// let _f1_id = m.create_function("f1", Type::Void, vec![]);
+    /// ```
     pub fn create_function(
         &mut self,
         name: &str,
@@ -27,20 +63,47 @@ impl Module {
         Function::new(self, name, ret_ty, params_ty)
     }
 
+    /// Add function to the receiver of this method.
+    /// See also [Function](../function/struct.Function.html) how this method is used.
     pub fn add_function(&mut self, f: Function) -> FunctionId {
         let id = self.functions.alloc(f);
         self.function_ref_mut(id).id = Some(id);
         id
     }
 
+    /// get a reference of the function that has the id.
+    ///
+    /// # Arguments
+    ///
+    /// * id ... an identifier of [Function](../function/struct.Function.html) what you want to reference.
     pub fn function_ref(&self, id: FunctionId) -> &Function {
         &self.functions[id]
     }
 
+    /// get a mutable reference of the function that has the id.
+    ///
+    /// # Arguments
+    ///
+    /// * id ... an identifier of [Function](../function/struct.Function.html) what you want to reference.
     pub fn function_ref_mut(&mut self, id: FunctionId) -> &mut Function {
         &mut self.functions[id]
     }
 
+    /// find a function that is defined in the module by name.
+    ///
+    /// # Examples
+    /// ```
+    /// use cilk::{module::Module, types::Type};
+    ///
+    /// let mut m1 = Module::new("owner");
+    /// let m2 = Module::new("other");
+    ///
+    /// let _f1_id = m.create_function("f1", Type::Void, vec![]);
+    ///
+    /// // check which module has the function.
+    /// assert!(m1.find_function("f1").is_some());
+    /// assert!(m2.find_function("f1").is_none())
+    /// ```
     pub fn find_function<'a, Name: Into<FunctionName<'a>>>(
         &self,
         name: Name,
