b84f336ad38f7200d2042fa99d1bd7ea16ade446
maekawatoshiki <konndennsa@gmail.com>
2020-08-04 19:08:14 +0900

    Implement assembler base

--- src/codegen/common/asm/assembler.rs
+++ src/codegen/common/asm/assembler.rs
@@ -1,5 +1,6 @@
 use crate::codegen::common::machine::{
-    basic_block::MachineBasicBlock, function::MachineFunction, module::MachineModule,
+    basic_block::MachineBasicBlock, function::MachineFunction, inst::MachineInst,
+    module::MachineModule,
 };
 
 pub struct Assembler<'a> {
@@ -15,6 +16,18 @@ pub struct BlockAssembler<'a> {
     module: &'a MachineModule,
     function: &'a MachineFunction,
     block: &'a MachineBasicBlock,
+    stream: InstructionStream,
+}
+
+pub struct InstAssembler<'a> {
+    module: &'a MachineModule,
+    function: &'a MachineFunction,
+    block: &'a MachineBasicBlock,
+    inst: &'a MachineInst,
+    stream: &'a mut InstructionStream,
+}
+
+pub struct InstructionStream {
     bytes: Vec<u8>,
 }
 
@@ -36,7 +49,7 @@ impl<'a> FunctionAssembler<'a> {
     }
 
     pub fn assemble(&mut self) {
-        for (block_id, block, iseq) in self.function.body.mbb_iter() {
+        for (block_id, block) in self.function.body.basic_blocks.id_and_block() {
             let block_asmer = BlockAssembler::new(self.module, self.function, block);
         }
     }
@@ -52,7 +65,44 @@ impl<'a> BlockAssembler<'a> {
             module,
             function,
             block,
-            bytes: vec![],
+            stream: InstructionStream::new(),
         }
+    }
+
+    pub fn assemble(&mut self) {
+        for &id in &*self.block.iseq_ref() {
+            let inst = &self.function.body.inst_arena[id];
+            let inst_asmer = InstAssembler::new(
+                self.module,
+                self.function,
+                self.block,
+                &mut self.stream,
+                inst,
+            );
+        }
+    }
+}
+
+impl<'a> InstAssembler<'a> {
+    pub fn new(
+        module: &'a MachineModule,
+        function: &'a MachineFunction,
+        block: &'a MachineBasicBlock,
+        stream: &'a mut InstructionStream,
+        inst: &'a MachineInst,
+    ) -> Self {
+        Self {
+            module,
+            function,
+            block,
+            inst,
+            stream,
+        }
+    }
+}
+
+impl InstructionStream {
+    pub fn new() -> Self {
+        Self { bytes: vec![] }
     }
 }
