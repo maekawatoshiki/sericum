00ab20bb042f6961e6a1cdfff987e5e69d779940
maekawatoshiki <konndennsa@gmail.com>
2020-09-14 22:39:28 +0900

    Update Dockerfile

--- run_kcov.sh
+++ run_kcov.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd minilang
-cargo test
+RUST_BACKTRACE=full cargo test -- --test-threads=1
 cd ..
-cargo test --features x86_64
+RUST_BACKTRACE=full cargo test --features x86_64 -- --test-threads=1
 REPORT=$(find ./target/debug -maxdepth 2 -regex '.+/deps/.*' -a ! -regex '.+\.\(d\|rlib\|rmeta\|so\)')
 for file in $REPORT; do  
   echo $file
