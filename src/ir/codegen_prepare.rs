21c94171c758f66102a1764315deb91e31fa4cdb
maekawatoshiki <konndennsa@gmail.com>
2020-09-26 19:32:56 +0900

    CodegenPrepare: sink more geps

--- src/ir/codegen_prepare.rs
+++ src/ir/codegen_prepare.rs
@@ -29,7 +29,7 @@ impl<'a> CodegenPrepareOnFunction<'a> {
         for (_, block) in &self.func.basic_blocks.arena {
             for inst_id in block.iseq.borrow().iter().map(|i| i.as_instruction().id) {
                 let inst = &self.func.inst_table[inst_id];
-                if inst.opcode == Opcode::GetElementPtr && inst.has_one_use() {
+                if inst.opcode == Opcode::GetElementPtr && self.able_to_be_sunk(inst) {
                     geps_to_be_sunk.push(inst_id);
                 }
             }
@@ -43,14 +43,33 @@ impl<'a> CodegenPrepareOnFunction<'a> {
         for gep_id in geps_to_be_sunk {
             let gep_val = self.func.remove_inst_from_block(gep_id);
             let gep = &self.func.inst_table[gep_id];
-            assert_eq!(gep.users.borrow().len(), 1);
-            let user_id = *gep.users.borrow().iter().next().unwrap();
+            let user_id = *gep
+                .users
+                .borrow()
+                .iter()
+                .min_by(|&&x, &&y| {
+                    self.func
+                        .find_inst_pos(x)
+                        .unwrap()
+                        .1
+                        .cmp(&self.func.find_inst_pos(y).unwrap().1)
+                })
+                .unwrap();
             let user_parent = self.func.inst_table[user_id].parent;
             let gep = &mut self.func.inst_table[gep_id];
             gep.parent = user_parent;
             let mut builder = Builder::new(FunctionEntity(self.func));
             builder.set_insert_point_before_inst(user_id);
             builder.insert(gep_val);
+        }
+    }
+
+    fn able_to_be_sunk(&self, inst: &Instruction) -> bool {
+        inst.has_one_use() || {
+            inst.users
+                .borrow()
+                .windows(2)
+                .all(|us| self.func.inst_table[us[0]].parent == self.func.inst_table[us[1]].parent)
         }
     }
 }
