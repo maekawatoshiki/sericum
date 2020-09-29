376f2cf57a22f4f7be713a7a3d24651c1b940c7c
maekawatoshiki <konndennsa@gmail.com>
2020-09-29 21:42:17 +0900

    Add Env, AST body

--- cilkcc/src/ast.rs
+++ cilkcc/src/ast.rs
@@ -7,4 +7,49 @@ pub struct AST {
 }
 
 #[derive(Debug, Clone)]
-pub enum Kind {}
+pub enum Kind {
+    Int { n: i64, bits: u8 },
+    Float(f64),
+    Char(u8),
+    String(String),
+    UnaryOp(UnaryOp, Box<AST>),
+    BinaryOp(BinaryOp, Box<AST>, Box<AST>),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum UnaryOp {
+    LogicalNot,
+    BitwiseNot,
+    Minus,
+    PreInc,
+    PreDec,
+    PostInc,
+    PostDec,
+    Deref,
+    Addr,
+    Sizeof,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum BinaryOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Rem,
+    And,
+    Or,
+    Xor,
+    LogicalAnd,
+    LogicalOr,
+    Eq,
+    Ne,
+    Lt,
+    Gt,
+    Le,
+    Ge,
+    Shl,
+    Shr,
+    Comma,
+    // Assign,
+}
--- cilkcc/src/parser.rs
+++ cilkcc/src/parser.rs
@@ -1,16 +1,20 @@
 use super::ast::AST;
 use super::lexer::{Error, Lexer, Result};
+use rustc_hash::FxHashMap;
+use std::collections::VecDeque;
 
 pub struct Parser<'a> {
     pub lexer: &'a mut Lexer,
 }
+
+pub struct Env<T: Clone>(pub VecDeque<FxHashMap<String, T>>);
 
 impl<'a> Parser<'a> {
     pub fn new(lexer: &'a mut Lexer) -> Parser<'a> {
         Self { lexer }
     }
 
-    pub fn run(&mut self) -> Result<Vec<AST>> {
+    pub fn parse(&mut self) -> Result<Vec<AST>> {
         let mut node = vec![];
         while let Ok(n) = self.read_toplevel() {
             node.push(n);
@@ -18,7 +22,56 @@ impl<'a> Parser<'a> {
         Ok(node)
     }
 
+    pub fn parse_as_expr(&mut self) -> Result<AST> {
+        todo!()
+    }
+
     fn read_toplevel(&mut self) -> Result<AST> {
         todo!()
+    }
+}
+
+impl<T: Clone> Env<T> {
+    pub fn new() -> Env<T> {
+        let mut env = VecDeque::new();
+        env.push_back(FxHashMap::default());
+        Env(env)
+    }
+
+    pub fn push(&mut self) {
+        let localenv = (*self.0.back().unwrap()).clone();
+        self.0.push_back(localenv);
+    }
+
+    pub fn pop(&mut self) {
+        self.0.pop_back();
+    }
+
+    pub fn add(&mut self, name: String, val: T) {
+        self.0.back_mut().unwrap().insert(name, val);
+    }
+
+    pub fn add_global(&mut self, name: String, val: T) {
+        // self.0[0].insert(name.clone(), val.clone());
+        // self.0.back_mut().unwrap().insert(name, val);
+        for env in &mut self.0 {
+            env.insert(name.clone(), val.clone());
+        }
+    }
+
+    pub fn is_local(&self) -> bool {
+        self.0.len() > 1
+    }
+
+    pub fn back_mut(&mut self) -> Option<&mut FxHashMap<String, T>> {
+        self.0.back_mut()
+    }
+
+    pub fn get(&mut self, name: &str) -> Option<&T> {
+        self.0.back_mut().unwrap().get(name)
+    }
+
+    pub fn contains(&mut self, name: &str) -> bool {
+        self.0.back_mut().unwrap().contains_key(name)
     }
 }
